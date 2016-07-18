199f42aa85375dc57fd0706a69e8a7c77db3a9bc
tuber <btu@thoughtworks.com>
2016-07-18 09:33:32 +0800

    [tuber] install webpack

---
+++ app/app.js
@@ -0,0 +1,4 @@
+'use strict';
+import cats from './cats';
+
+console.log(cats);
---
+++ webpack.config.js
@@ -0,0 +1,15 @@
+'use strict';
+module.exports = {
+    entry: './src/app.js',
+    output: {
+        path: './bin',
+        filename: 'app.bundle.js'
+    },
+    module: {
+        loaders: [{
+            test: /\.js$/,
+            exclude: /node_modules/,
+            loader: 'babel-loader'
+        }]
+    }
+};
